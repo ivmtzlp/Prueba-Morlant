2fd3ecdd526083774da5360d4b81751dde6ed36c
ivmtzlp <167115740+ivmtzlp@users.noreply.github.com>
2024-04-15 11:39:25 -0600

    agregar r

---
+++ git_use.R
@@ -0,0 +1,33 @@
+
+#ejemplo git R
+
+
+library(usethis)
+library(gitcreds)
+
+#acceder a la cuenta git
+use_git_config(user.name= "ivmtzlp", user.email="soylivo@gmail.com")
+
+
+#set un branch de git
+git_default_branch_configure()
+
+#creando token
+create_github_token()
+
+
+
+gitcreds_get()
+
+usethis::gh_token_help()
+
+usethis::git_sitrep()
+
+gh::gh_whoami()
+
+#definior como la ruta de github
+credentials::set_github_pat()
+
+
+setwd("C:/Users/ivans/Desktop/Trab/Git")
+
